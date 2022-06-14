62f30238ed0fdeea79c42f95fd1b5f9da5e7b136
mrists <kalashnikoff.wh@gmail.com>
2022-06-14 22:17:05 +0300

    TODO frontend page

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* подключение и настройка внутристраничной навигации */
+$('#navbar').onePageNav({
+  currentClass: 'active',
+  scrollSpeed: 750,
+  easing: 'swing',
+  filter: ':not(.navbar-brand)'
+});
+/* подключение и настройка внутристраничной навигации */
