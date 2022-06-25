7e1b20448e051ed8ddb7da2f97e2300cafc0a8ed
mrists <kalashnikoff.wh@gmail.com>
2022-06-25 12:12:05 +0300

    Rewrite to class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,48 @@
-/* подключение и настройка внутристраничной навигации */
-$('#navbar').onePageNav({
-  currentClass: 'active',
-  scrollSpeed: 750,
-  easing: 'swing',
-  filter: ':not(.navbar-brand)'
-});
-/* подключение и настройка внутристраничной навигации */
+const navbarList = document.querySelector('#navbar__list');
+const navbarItems = document.querySelectorAll('.navbar__item');
+
+const screenHeight = window.screen.height;
+
+let sectionPosition = Math.round((window.scrollY + screenHeight) / screenHeight);
+navbarItems[sectionPosition - 1].classList.toggle('active');
+
+let prevPosition = null;
+let prevSectionPosition = 1;
+let prevItem = document.querySelector('.active');
+
+
+function checkUserPosition() {
+    if (window.scrollY !== prevPosition) {
+        sectionPosition = Math.round((window.scrollY + screenHeight) / screenHeight);
+        if (sectionPosition !== prevSectionPosition) {
+            navbarItems[sectionPosition - 1].classList.toggle('active');
+            if (prevItem && prevItem !== navbarItems[sectionPosition - 1]) prevItem.classList.toggle('active') ;    
+            prevItem = document.querySelector('.active'); 
+        }
+        prevSectionPosition = sectionPosition;
+    }
+
+    prevPosition = window.scrollY;
+};
+
+function goToNthSection(e) {
+    e.preventDefault();
+    let blockID = null;
+    
+    for (let i = 0; !blockID; i++) {
+        if (e.path[i].matches('.navbar__link')) {
+            blockID = e.path[i].getAttribute('href');
+        }
+    }
+
+    let scrollTarget = document.querySelector(blockID);
+    scrollTarget.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    }) - 100;
+
+    setInterval(() => checkUserPosition(), 300);
+};
+
+document.body.addEventListener('wheel', checkUserPosition);
+navbarList.addEventListener('click', goToNthSection);
