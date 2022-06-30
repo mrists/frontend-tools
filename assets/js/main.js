540e074596b09f74591a646e3eda415b6f39906d
mrists <kalashnikoff.wh@gmail.com>
2022-06-30 14:05:36 +0300

    configurable navbar, was rewritten to class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,59 @@
-const navbarList = document.querySelector('#navbar__list');
-const navbarItems = document.querySelectorAll('.navbar__item');
+import initNavbar from "./navbar.js";
 
-const screenHeight = window.screen.height;
+const navbar = new initNavbar();
 
-let sectionPosition = Math.round((window.scrollY + screenHeight) / screenHeight);
-navbarItems[sectionPosition - 1].classList.toggle('active');
-
-let prevPosition = null;
-let prevSectionPosition = 1;
-let prevItem = document.querySelector('.active');
+navbar.init();
 
 
-function checkUserPosition() {
-    if (window.scrollY !== prevPosition) {
-        sectionPosition = Math.round((window.scrollY + screenHeight) / screenHeight);
-        if (sectionPosition !== prevSectionPosition) {
-            navbarItems[sectionPosition - 1].classList.toggle('active');
-            if (prevItem && prevItem !== navbarItems[sectionPosition - 1]) prevItem.classList.toggle('active') ;    
-            prevItem = document.querySelector('.active'); 
-        }
-        prevSectionPosition = sectionPosition;
-    }
 
-    prevPosition = window.scrollY;
-};
+// const navbarList = document.querySelector('#navbar__list');
+// const navbarItems = document.querySelectorAll('.navbar__item');
 
-function goToNthSection(e) {
-    e.preventDefault();
-    let blockID = null;
+// const screenHeight = window.screen.height;
+
+// let sectionPosition = Math.round((window.scrollY + screenHeight) / screenHeight);
+// navbarItems[sectionPosition - 1].classList.toggle('active');
+
+// let prevPosition = null;
+// let intervalID = null;
+// let prevSectionPosition = 1;
+// let prevItem = document.querySelector('.active');
+
+
+// function checkUserPosition() {
+//     if (window.scrollY !== prevPosition) {
+//         sectionPosition = Math.round((window.scrollY + screenHeight) / screenHeight);
+//         if (sectionPosition !== prevSectionPosition) {
+//             navbarItems[sectionPosition - 1].classList.toggle('active');
+//             if (prevItem && prevItem !== navbarItems[sectionPosition - 1]) prevItem.classList.toggle('active') ;    
+//             prevItem = document.querySelector('.active'); 
+//         }
+//         prevSectionPosition = sectionPosition;
+//     }
+
+//     prevPosition = window.scrollY;
+// };
+
+// function goToNthSection(e) {
+//     e.preventDefault();
+//     let blockID = null;
     
-    for (let i = 0; !blockID; i++) {
-        if (e.path[i].matches('.navbar__link')) {
-            blockID = e.path[i].getAttribute('href');
-        }
-    }
+//     for (let i = 0; !blockID; i++) {
+//         if (e.path[i].matches('.navbar__link')) {
+//             blockID = e.path[i].getAttribute('href');
+//         }
+//     }
 
-    let scrollTarget = document.querySelector(blockID);
-    scrollTarget.scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    });
+//     let scrollTarget = document.querySelector(blockID);
+//     scrollTarget.scrollIntoView({
+//       behavior: "smooth",
+//       block: "start"
+//     });
 
-    setInterval(() => checkUserPosition(), 300);
-};
 
-document.body.addEventListener('wheel', checkUserPosition);
-navbarList.addEventListener('click', goToNthSection);
+//     intervalID = setInterval(() => checkUserPosition(), 100);
+//     setTimeout(() => clearInterval(intervalID), 700);
+// };
+
+// document.body.addEventListener('wheel', checkUserPosition);
+// navbarList.addEventListener('click', goToNthSection);
