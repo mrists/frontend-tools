5c47eece7bed86d931e5d2ee0984476f54104267
mrists <kalashnik.offwh@gmail.com>
2023-04-23 16:56:40 +0300

    fixed bug with navigation, fixed links

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,73 +1,98 @@
 export default class initNavbar {
-    constructor(params) {
-        const settings = {...{navbarListId: 'navbar__list', navbarItemsClass: 'navbar__item', activeSectionClass: 'active', clearIntervalTime: 700}, ...params};
-        this.navbarList = document.querySelector(`#${settings.navbarListId}`);
-        this.navbarItems = document.querySelectorAll(`.${settings.navbarItemsClass}`);
-        this.activeClass = settings.activeSectionClass;
-        this.clearIntervalTime = settings.clearIntervalTime;
-    }
-    
-    _initProps() {
-        this.screenHeight = window.screen.height;
-        this.prevPosition = null;
-        this.intervalID = null;
-        this.prevSectionPosition = 1;
-    }
-    
-    _initDefaults() {
-        this.sectionPosition = Math.round((window.scrollY + this.screenHeight) / this.screenHeight);
-        this.navbarItems[this.sectionPosition - 1].classList.toggle(this.activeClass);
-        this.prevItem = document.querySelector(`.${this.activeClass}`);
-    }
+	constructor(params) {
+		const settings = {
+			...{
+				navbarListId: 'navbar__list',
+				navbarItemsClass: 'navbar__item',
+				activeSectionClass: 'active',
+				clearIntervalTime: 700,
+			},
+			...params,
+		};
+		this.navbarList = document.querySelector(`#${settings.navbarListId}`);
+		this.navbarItems = document.querySelectorAll(`.${settings.navbarItemsClass}`);
+		this.activeClass = settings.activeSectionClass;
+		this.clearIntervalTime = settings.clearIntervalTime;
+	}
 
-    _initListeners() {
-        document.body.addEventListener('wheel', this._checkUserPosition.bind(this));
-        this.navbarList.addEventListener('click', this._goToNthSection.bind(this));
-    }
+	_initProps() {
+		this.screenHeight = window.screen.height;
+		this.prevPosition = null;
+		this.intervalID = null;
+		this.prevSectionPosition = 1;
+	}
 
-    _checkUserPosition() {
-        if (window.scrollY !== this.prevPosition) {
-            this.screenHeight = window.screen.height;
-            this.sectionPosition = Math.round((window.scrollY + this.screenHeight) / this.screenHeight);
-            if (this.sectionPosition !== this.prevSectionPosition) {
-                this.navbarItems[this.sectionPosition - 1].classList.toggle(this.activeClass);
-                if (this.prevItem && this.prevItem !== this.navbarItems[this.sectionPosition - 1]) this.prevItem.classList.toggle(this.activeClass);    
-                this.prevItem = document.querySelector(`.${this.activeClass}`); 
-            }
+	_initDefaults() {
+		this.sectionPosition = Math.round((window.scrollY + this.screenHeight) / this.screenHeight);
+		this.navbarItems[this.sectionPosition - 1].classList.toggle(this.activeClass);
+		this.prevItem = document.querySelector(`.${this.activeClass}`);
+	}
 
-            this.prevSectionPosition = this.sectionPosition;
-        }
-    
-        this.prevPosition = window.scrollY;
-    }
+	_initListeners() {
+		document.body.addEventListener('wheel', this._checkUserPosition.bind(this));
+		this.navbarList.addEventListener('click', this._goToNthSection.bind(this));
+	}
 
-    _goToNthSection(e) {
-        e.preventDefault();
-        this.blockID = null;
-        
-        for (let i = 0; !this.blockID; i++) {
-            if (e.path[i].matches('.navbar__link')) {
-                this.blockID = e.path[i].getAttribute('href');
-            }
-        }
-    
-        this.scrollTarget = document.querySelector(this.blockID);
-        this.scrollTarget.scrollIntoView({
-          behavior: "smooth",
-          block: "start"
-        });
-    
-        this.intervalID = setInterval(() => this._checkUserPosition(), 100);
-        setTimeout(() => clearInterval(this.intervalID), this.clearIntervalTime);
-    }
+	_checkUserPosition() {
+		if (window.scrollY !== this.prevPosition) {
+			this.screenHeight = window.screen.height;
+			this.sectionPosition = Math.round((window.scrollY + this.screenHeight) / this.screenHeight);
+			if (this.sectionPosition !== this.prevSectionPosition) {
+				this.navbarItems[this.sectionPosition - 1].classList.toggle(this.activeClass);
+				if (this.prevItem && this.prevItem !== this.navbarItems[this.sectionPosition - 1])
+					this.prevItem.classList.toggle(this.activeClass);
+				this.prevItem = document.querySelector(`.${this.activeClass}`);
+			}
 
+			this.prevSectionPosition = this.sectionPosition;
+		}
 
-    init() {
-        this._initProps();
-        this._initDefaults();
-        this._initListeners();
-    }
+		this.prevPosition = window.scrollY;
+	}
+
+	_goToNthSection(e) {
+		e.preventDefault();
+		this.blockID = null;
+
+		const sectionNumber = +e.target.getAttribute('data-scroll-to');
+
+		switch (sectionNumber) {
+			case 0:
+				this.blockID = '#header';
+				break;
+			case 1:
+				this.blockID = '#html';
+				break;
+			case 2:
+				this.blockID = '#css';
+				break;
+			case 3:
+				this.blockID = '#javascript';
+				break;
+			case 4:
+				this.blockID = '#frontend';
+				break;
+		}
+
+		document.body.style.overflow = 'hidden';
+
+		this.scrollTarget = document.querySelector(this.blockID);
+		this.scrollTarget.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		});
+
+		this.intervalID = setInterval(() => this._checkUserPosition(), 100);
+		setTimeout(() => clearInterval(this.intervalID), this.clearIntervalTime);
+
+		setTimeout(() => {
+			document.body.removeAttribute('style');
+		}, this.clearIntervalTime);
+	}
+
+	init() {
+		this._initProps();
+		this._initDefaults();
+		this._initListeners();
+	}
 }
-
-
-
